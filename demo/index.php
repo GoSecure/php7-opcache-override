6ed31d3fde937d9a6370ac2df6b783cca4585a23
Corb3nik <ian@subnet192.com>
2016-04-26 10:20:42 -0400

    First version of demo website

---
+++ demo/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Vulnerable Website</title>
+  <head>
+
+<?php
+
+  $uploaddir = 'uploads/';
+  if (isset($_FILES['file'])) {
+    $file = $_FILES['file'];
+    $filename = $file['name'];
+
+    $dest = $uploaddir . $_POST['path'] . $filename;
+    $contents = file_get_contents($file['tmp_name']);
+    if (file_put_contents($dest, $contents))
+      echo "Upload successful, view your file here : <a href='/uploads/" . $filename . "'>$filename.</a>";
+    else
+      echo "<h3 style='color:red'>Permission Denied : You can't write in this folder.</h3>";
+  }
+
+?>
+
+  <body>
+    <h1>Upload a file</h1>
+    <p>Upload a file at the path of your choice</p>
+
+    <form method="POST" enctype='multipart/form-data' action='.'>
+      <label>Path : </label>
+      <input type="text" name="path" placeholder="scenario_1/uploads/" style='width:800px'/>
+
+      <br/>
+
+      <input type="hidden" name="MAX_FILE_SIZE" value="2000000" />
+      <label>File : </label>
+      <input type="file" name="file"/>
+
+      <br/>
+      <input type="submit" value="Upload">
+    </form>
+
+    <a href="/scenario_1/override_me.php">override_me</a>
+  </body>
+</html>
+
---
+++ demo/override_me.php
@@ -0,0 +1,3 @@
+<?php
+  echo "Try to override me";
+?>
