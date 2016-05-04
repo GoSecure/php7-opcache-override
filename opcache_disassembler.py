b2575c2cdcc78ed8f657ce720f7655ec00500b5c
Corb3nik <ian@subnet192.com>
2016-05-04 14:09:36 -0400

    Adds first version of OPcache disassembler, parser and malware detector

---
+++ opcache_disassembler.py
@@ -0,0 +1,261 @@
+#!/usr/bin/env python
+
+from __future__ import unicode_literals
+from opcache_parser import *
+from treelib import Node, Tree
+from termcolor import colored
+import re
+import sys
+
+class OPcode(Tree):
+    """ Tree representation of an OPcode """
+
+    def __init__(self, id, opcode, op_array, context):
+        """ Create a tree representaiton of an Opcode
+
+            Arguments :
+                id : The id representing the new OPcode
+                opcode : An OPcode struct
+                op_array : The op_array of the OPcode
+                context : An OPcacheParser instance
+        """
+
+        Tree.__init__(self)
+
+        # Identifier to be used by the tree and nodes
+        id_with_hash = str(hash(str(op_array))) + "_" + id
+
+        # OP name
+        op = OPcodeParser.get_opcode_name(opcode['opcode'])
+
+        # Parser
+        context = OPcodeParser(context)
+
+        # Parse operands and result
+        (op1, op2, result) = context.parse_operands(opcode, op_array)
+
+        # Create nodes
+        op1_node = Node("Operand 1: " + op1, id_with_hash + "_op1")
+        op2_node = Node("Operand 2: " + op2, id_with_hash + "_op2")
+        result_node = Node("Result: " + result, id_with_hash + "_result")
+
+        # Link nodes to tree
+        self.create_node(id + ": " + op, id_with_hash + "_opcode")
+        self.add_node(op1_node, parent=id_with_hash + "_opcode")
+        self.add_node(op2_node, parent=id_with_hash + "_opcode")
+        self.add_node(result_node, parent=id_with_hash + "_opcode")
+
+class OPcacheDisassembler():
+    """ Disassembles a given file """
+
+    def syntax_highlight(self, line):
+        """ Syntax highlighting for a disassembled Opcache line """
+
+        # JMP
+        if "JMP" in line and "->" in line:
+            line = re.sub(" (JMP.+)\(", colored(' \\1', 'green') + "(", line)
+
+        # Variables
+        line = re.sub("(\$\d+)", colored('\\1', 'cyan'), line)
+
+        # Temporary variables
+        line = re.sub("(~\d+)", colored('\\1', 'red'), line)
+
+        # Compiled variables
+        line = re.sub("(!\d+)", colored('\\1', 'yellow'), line)
+
+        # Strings
+        line = re.sub("'(.*)'", colored("'\\1'", 'blue'), line)
+
+        return line
+
+
+    def convert_branch_to_pseudo_code(self, root, nid, indentation = 0):
+        """ Convert a branch to readable code
+
+            Arguments :
+                root : The root of the AST
+                nid : The node ID of the branch to convert, usually 'main_op_array', 'function_table'  or 'class_table'
+                indentation : The indentation to use for printing
+        """
+
+        line = ""
+        lineno = 0
+        for child_nid in root[nid].fpointer:
+
+            # Check if the node is an opcode
+            if "opcode" in child_nid:
+                line += " " * indentation + self.convert_opcode_to_line(root, root[child_nid], lineno) + "\n"
+                lineno += 1
+
+            # Else, it's a function/class
+            else:
+                if "_class_function" in child_nid:
+                    prefix = "function "
+
+                elif "_class" in child_nid:
+                    prefix = "class "
+                else:
+                    prefix = "function "
+
+                line += " " * indentation + prefix + root[child_nid].tag + "() {\n"
+
+                indentation += 2
+                line += self.convert_branch_to_pseudo_code(root, child_nid, indentation) + "\n"
+                indentation -= 2
+
+                line += " " *  indentation + "}"
+
+        return line
+
+    def convert_opcode_to_line(self, root, opcode, lineno = 0):
+        """ Convert an opcode to a pseudo code using the following syntax : '#1 result = function(param1, param2)'
+
+                Arguments :
+                    root : The root of the AST
+                    opcode : The OPcode to convert
+                    lineno : The line number to use
+        """
+
+        # List of children of an OPcode
+        children = opcode.fpointer
+
+        # OP name
+        op_name = opcode.tag.split(":")[1].strip()
+
+        # Parameters and result
+        param1 = root[children[0]].tag.split(":", 1)[1].strip()
+        param2 = root[children[1]].tag.split(":", 1)[1].strip()
+        result = root[children[2]].tag.split(":", 1)[1].strip()
+
+        # Prepend the line number
+        line = "#" + str(lineno) + " "
+
+        # Hide the result part if it's not used
+        if result != "None":
+            line += result + " = "
+
+        # Final formatting
+        line += "{0}({1}, {2});".format(op_name, param1, param2)
+
+        # Syntax highlighting
+        return self.syntax_highlight(line)
+
+    def create_ast(self, filename):
+        """ Create an ast for a given file
+
+            Arguments :
+                filename : The name of the file to parse
+        """
+
+        # Create parser
+        opcache = OPcacheParser(filename)
+
+        # Create syntax tree
+        ast = Tree()
+        ast.create_node("script", "script")
+        ast.create_node("main_op_array", "main_op_array", parent="script")
+        ast.create_node("function_table", "function_table", parent="script")
+        ast.create_node("class_table", "class_table", parent="script")
+
+        # Get main structures
+        main_op_array = opcache['script']['main_op_array']
+        functions = opcache['script']['function_table']['buckets']
+        classes = opcache['script']['class_table']['buckets']
+
+        # Main OP array
+        for idx, opcode in enumerate(main_op_array['opcodes']):
+            opcode = OPcode(str(idx), opcode, main_op_array, opcache)
+            ast.paste("main_op_array", opcode)
+
+        # Function Table
+        for function in functions:
+
+            # Create function node
+            function_name = function['key']['val']
+            function_id = function_name + "_function"
+            ast.create_node(function_name, function_id, parent="function_table")
+
+            # Iterate over opcodes
+            op_array = function['val']['op_array']
+            for idx, opcode in enumerate(op_array['opcodes']):
+                opcode = OPcode(str(idx), opcode, op_array, opcache)
+                ast.paste(function_id, opcode)
+
+        # Class Table
+        for class_ in classes:
+
+            # Check for real classes
+            if class_['val']['u1']['type'] == IS_PTR:
+
+                # Create class node
+                class_name = class_['key']['val']
+                class_id = class_name + "_class"
+                ast.create_node(class_name, class_id, parent="class_table")
+
+                # Function Table
+                for function in class_['val']['class']['function_table']['buckets']:
+
+                    # Create function node
+                    function_name = function['key']['val']
+                    class_function_id = function_name + "_class_function"
+                    ast.create_node(function_name, class_function_id, parent=class_id)
+
+                    # Iterate over opcodes
+                    for idx, opcode in enumerate(function['val']['op_array']['opcodes']):
+                        opcode = OPcode(str(idx), opcode, function['val']['op_array'], opcache)
+                        ast.paste(class_function_id, opcode)
+
+
+        return ast
+
+    def print_pseudo_code(self, ast):
+        """ Print the pseudo code
+
+            Arguments :
+                ast : The syntax tree to print
+        """
+
+        print self.convert_branch_to_pseudo_code(ast, 'class_table', 0)
+        print self.convert_branch_to_pseudo_code(ast, 'function_table', 0)
+        print self.convert_branch_to_pseudo_code(ast, 'main_op_array', 0)
+
+    def print_syntax_tree(self, ast):
+        """ Print the syntax tree
+
+            Arguments :
+                ast : The syntax tree to print
+        """
+
+        ast.show(key=lambda a: "")
+
+def show_help():
+    """ Show the help menu """
+
+    print "Usage : {0} [-tc] [file]".format(sys.argv[0])
+    print " " * 4 + "-t Print syntax tree"
+    print " " * 4 + "-c Print pseudocode"
+
+if __name__ == "__main__":
+
+    show_pseudo_code = False
+    show_syntax_tree = False
+
+    if len(sys.argv) < 3:
+        show_help()
+        exit(0)
+    else:
+        if '-c' in sys.argv:
+            show_pseudo_code = True
+
+        if '-t' in sys.argv:
+            show_syntax_tree = True
+
+    disassembler = OPcacheDisassembler()
+    ast = disassembler.create_ast(sys.argv[len(sys.argv) - 1])
+
+    if show_syntax_tree:
+        disassembler.print_syntax_tree(ast)
+
+    if show_pseudo_code:
+        disassembler.print_pseudo_code(ast)
---
+++ opcache_parser.py
@@ -0,0 +1,351 @@
+#!/usr/bin/env python
+
+from construct import *
+from definitions import *
+
+def unserialize_zend_function():
+    return Zend_Op_Array("op_array")
+
+def unserialize_class():
+    return Zend_Class_Entry("class")
+
+def Empty():
+    return Struct("Empty")
+
+def Z_Val(name, callback = None, unserialize = True):
+
+    if not callback:
+        callback = Empty
+
+    return Struct(name,
+                  Zend_Value("value"),
+                  Struct("u1",
+                         Byte("type"),
+                         Byte("type_flags"),
+                         Byte("const_flags"),
+                         Byte("reserved")
+                  ),
+                  ULInt32("u2"),
+
+                  If(lambda z: z.u1.type == 6 and unserialize,
+                     Pointer(lambda z: z.value.w1 + Struct.sizeof(Meta), Zend_String("string"))
+                  ),
+                  If(lambda z: z.u1.type == 17 and unserialize,
+                     Pointer(lambda z: z.value.w1 + Struct.sizeof(Meta), callback()))
+                  )
+
+def Pointer_To(name, structure):
+    structure.name = "value"
+    return Struct(name,
+                  ULInt32("position"),
+                  IfThenElse(structure.name, lambda z: z.position == 0,
+                            Empty(),
+                            Pointer(lambda z: z.position + Struct.sizeof(Meta), structure))
+                  )
+
+def Zend_Class_Entry(name):
+    return Struct(name,
+                  Bytes("padding", 3),
+                  Byte("type"),
+                  Pointer_To("name", Zend_String("name")),
+                  ULInt32("parent_pos"),
+                  ULInt32("refcount"),
+                  ULInt32("ce_flags"),
+                  ULInt32("default_properties_count"),
+                  ULInt32("default_static_members_count"),
+                  ULInt32("default_properties_table_pos"),
+                  ULInt32("default_static_members_table_pos"),
+                  ULInt32("static_members_table_pos"),
+                  Hash_Table("function_table", unserialize_zend_function),
+                  Hash_Table("properties_table"),
+                  Hash_Table("constants_table"),
+                  Pointer_To("constructor", Zend_Function("constructor")))
+
+def Zend_Function(name):
+    return Struct(name,
+                  Byte("type"),
+                  Bytes("arg_flags", 3),
+                  ULInt32("fn_flags"),
+                  Pointer_To("function_name", Zend_String("function_name")),
+                  ULInt32("scope_pos"),
+                  ULInt32("prototype_pos"),
+                  ULInt32("num_args"),
+                  ULInt32("required_num_args"),
+                  Pointer_To("arg_info", Zend_String("arg_info")))
+
+def Bucket(name, callback = None):
+    return Struct(name,
+                  Z_Val("val", callback),
+                  ULInt32("h"),
+                  ULInt32("key_pos"),
+                  If(lambda z: z.val.u1.type != 0,
+                     Pointer(lambda z: z.key_pos + Struct.sizeof(Meta), Zend_String("key")))
+                  )
+
+def Hash_Table(name, callback = None):
+    return Struct(name,
+                  Zend_Refcounted_H("gc"),
+                  ULInt32("flags"),
+                  ULInt32("nTableMask"),
+                  ULInt32("bucket_pos"),
+                  ULInt32("nNumUsed"),
+                  ULInt32("nNumOfElements"),
+                  ULInt32("nTableSize"),
+                  ULInt32("nInternalPointer"),
+                  ULInt32("nNextFreeElement"),
+                  ULInt32("pDestructor"),
+                  Pointer(lambda z: z.bucket_pos + Struct.sizeof(Meta),
+                    Array(lambda z: z.nNumUsed,
+                      Bucket("buckets", callback)
+                  )))
+
+def Zend_Value(name):
+    return Struct(name,
+                  ULInt32("w1"),
+                  ULInt32("w2"))
+
+
+def Zend_Refcounted_H(name):
+    return  Struct(name,
+                   ULInt32("refcount"),
+                   ULInt32("typeinfo"))
+
+
+def Zend_String(name):
+    return Struct(name,
+                  Zend_Refcounted_H("gc"),
+                  ULInt32("h"),
+                  ULInt32("len"),
+                  String("val", lambda zs: zs.len))
+
+def Zend_Arg_Info(name):
+    return Struct(name,
+                  Pointer_To("name", Zend_String("name")),
+                  Pointer_To("class_name", Zend_String("class_name")),
+                  Byte("type_hint"),
+                  Byte("pass_by_reference"),
+                  Byte("allow_null"),
+                  Byte("is_variadic"))
+
+def Z_Node_Op(name):
+    return Struct(name,
+                  ULInt32("val"))
+
+def Zend_Op(name):
+    return Struct(name,
+                  ULInt32("handler"),
+                  Z_Node_Op("op1"),
+                  Z_Node_Op("op2"),
+                  Z_Node_Op("result"),
+                  ULInt32("extended_value"),
+                  ULInt32("lineno"),
+                  Byte("opcode"),
+                  Byte("op1_type"),
+                  Byte("op2_type"),
+                  Byte("result_type"))
+
+def Zend_Op_Array(name):
+    return Struct(name,
+                    Byte("type"),
+                    Bytes("arg_flags", 3),
+                    ULInt32("fn_flags"),
+                    Pointer_To("function_name", Zend_String("function_name")),
+                    ULInt32("scope_pos"),
+                    Pointer_To("prototype", Zend_String("prototype")),
+                    ULInt32("num_args"),
+                    ULInt32("required_num_args"),
+                    Pointer_To("arg_info", Zend_Arg_Info("arg_info")),
+                    ULInt32("refcount"),
+                    ULInt32("this_var"),
+                    ULInt32("last"),
+                    ULInt32("opcodes_pos"),
+                    Pointer(lambda z: z.opcodes_pos + Struct.sizeof(Meta),
+                            Array(lambda z: z.last,
+                                  Zend_Op("opcodes")
+                            )
+                    ),
+                    ULInt32("last_var"),
+                    ULInt32("T"),
+                    ULInt32("vars_pos_pos"),
+                    Pointer(lambda z: z.vars_pos_pos + Struct.sizeof(Meta),
+                            Array(lambda z: z.last_var,
+                                Struct("vars",
+                                       ULInt32("pos"),
+                                       Pointer(lambda v: v.pos + Struct.sizeof(Meta), Zend_String("var")))
+                            )
+                    ),
+                    ULInt32("last_live_range"),
+                    ULInt32("last_try_catch"),
+                    ULInt32("live_range_pos"),
+                    ULInt32("try_catch_array_pos"),
+                    Pointer_To("static_variables", Hash_Table("static_variables")),
+                    Pointer_To("filename", Zend_String("filename")),
+                    ULInt32("line_start"),
+                    ULInt32("line_end"),
+                    Pointer_To("doc_comment", Zend_String("doc_comment")),
+                    ULInt32("early_binding"),
+                    ULInt32("last_literals"),
+                    ULInt32("literals_pos"),
+                    Pointer(lambda z: z.literals_pos + Struct.sizeof(Meta),
+                            Array(lambda z: z.last_literals,
+                                  Z_Val("literals")
+                            )
+                    ),
+                    ULInt32("cache_size"),
+                    ULInt32("runtime_size"),
+                    Array(4, ULInt32("reserved")))
+
+Script = Struct("script",
+                Pointer_To("filename", Zend_String("filename")),
+                Zend_Op_Array("main_op_array"),
+                Hash_Table("function_table", unserialize_zend_function),
+                Hash_Table("class_table", unserialize_class))
+
+Meta = Struct("meta",
+              String("magic", 8),
+              String("system_id", 32),
+              ULInt32("mem_size"),
+              ULInt32("str_size"),
+              ULInt32("script_offset"),
+              ULInt32("timestamp"),
+              ULInt32("checksum"))
+
+OPcache = Struct("OPcache",
+                 Meta,
+                 Script)
+
+class OPcodeParser():
+    """ Parser for everything related to opcodes """
+
+    @staticmethod
+    def get_opcode_name(opcode):
+        """ Get the name for a given op number """
+
+        return OPCODES[opcode]
+
+    def __init__(self, opcache):
+        """ Keep track of the file stream for parsing
+
+            Arguments :
+                opcache : An OPcache instance to be used as context
+        """
+
+        self.stream = opcache.stream
+
+    def parse_jmp(self, opcode, op_array):
+        """ Parse jump instructions
+
+            Arguments :
+                opcode : An opcode struct
+                op_array : The op array of the opcode
+        """
+
+        zend_op_size = Struct.sizeof(Zend_Op(""))
+        op1_val = opcode['op1']['val']
+        op2_val = opcode['op2']['val']
+        opcodes_pos = op_array['opcodes_pos']
+
+        # Unconditional jump (no second operand)
+        if opcode['opcode'] == 42:
+            op1 = "->" + str((op1_val - opcodes_pos) / zend_op_size)
+            op2 = "None"
+
+        # Other jump instructions
+        else:
+            op1 = self.parse_zval(op2_val, opcode['op1_type'])
+            op2 = "->" + str((op2_val - opcodes_pos) / zend_op_size)
+
+        return (op1, op2, "None")
+
+    def parse_operands(self, opcode, op_array):
+        """ Parse the two operands and the result
+
+            Arguments :
+                opcode : An opcode struct
+                op_array : The op array of the opcode
+        """
+
+        # OP number and name
+        op_no = opcode['opcode']
+
+        # Treat jump instructions differently
+        if 42 <= op_no and op_no <= 47:
+            (op1, op2, result) = self.parse_jmp(opcode, op_array)
+
+        else:
+            op1 = self.parse_zval(opcode['op1'].val, opcode['op1_type'])
+            op2 = self.parse_zval(opcode['op2'].val, opcode['op2_type'])
+            result = self.parse_zval(opcode['result'].val, opcode['result_type'])
+
+        return (op1, op2, result)
+
+    def parse_zval(self, offset, op_type):
+        """ Parse a zval at a offset for a given operand type
+
+            Arguments :
+                offset : The offset of the zval
+                op_type : The type of the operand
+        """
+
+        # Size to add to the offset
+        size_of_meta = Struct.sizeof(Meta)
+
+        # If the offset is invalid, consider the operand as unused
+        try:
+            zval = Z_Val("val", unserialize=False).parse(self.stream[offset + size_of_meta:])
+        except:
+            return "None"
+
+        # Get the type of the z_val and the two possible values
+        type = zval['u1']['type']
+        w1 = zval['value']['w1']
+        w2 = zval['value']['w2']
+
+        # Interpret the z_val
+        if op_type == IS_CONST:
+            if type == IS_STRING:
+                return repr(Zend_String("val").parse(self.stream[w1 + size_of_meta:])['val'])
+
+            if type == IS_LONG:
+                return str(w1)
+
+            if type == IS_NULL:
+                return "None"
+
+        if op_type == IS_TMP_VAR:
+            return "~" + str(w2)
+
+        if op_type == IS_VAR:
+            return "$" + str(w2)
+
+        if op_type == IS_UNUSED:
+            return "None"
+
+        if op_type == IS_CV:
+            return "!" + str(w2)
+
+        return "None"
+
+
+
+class OPcacheParser():
+    """ Parses a given OPcache file """
+
+    def __init__(self, file_path):
+        """ Parses a given OPcache file
+            Arguments :
+                file_path : The path of the file to parse
+        """
+
+        with open(file_path, "r") as file:
+            self.stream = file.read()
+
+        self.parsed = OPcacheParser.parse_stream(self.stream)
+
+    def __getitem__(self, index):
+        return self.parsed[index]
+
+    @staticmethod
+    def parse_stream(stream):
+
+        return OPcache.parse(stream)
