0ded3e1d2aac5b9e1923498564f705375f375a03
Wang Yihang <wangyihanger@gmail.com>
2021-05-25 22:18:23 +0800

    Fit `system_id_scraper.py` for python3

--- system_id_scraper.py
+++ system_id_scraper.py
@@ -6,10 +6,14 @@ import sys
 from packaging import version
 import re
 import requests
-from md5 import md5
+
+def md5(data):
+    if type(data) is str:
+        data = bytes(data, encoding='utf-8')
+    return __import__('hashlib').md5(data).hexdigest()
 
 if len(sys.argv) != 2:
-    print sys.argv[0] + " [file|URL]"
+    print(sys.argv[0] + " [file|URL]")
     exit(0)
 
 if (sys.argv[1].startswith("http")):
@@ -26,23 +30,22 @@ if php_version == None:
     php_version = re.search('<h1 class="p">PHP Version (.*)', text)
 
 if php_version == None:
-    print "No PHP version found, is this a phpinfo file?"
+    print("No PHP version found, is this a phpinfo file?")
     exit(0)
 
 php_version = php_version.group(1)
 php_greater_74 = (version.parse("7.4.0") < version.parse(php_version.split("-")[0]))
-
 # Zend Extension Build ID
 zend_extension_id = re.search('<tr><td class="e">Zend Extension Build </td><td class="v">(.*) </td></tr>', text)
 if zend_extension_id == None:
-    print "No Zend Extension Build found."
+    print("No Zend Extension Build found.")
     exit(0)
 zend_extension_id = zend_extension_id.group(1)
 
 # Architecture
 architecture = re.search('<tr><td class="e">System </td><td class="v">(.*) </td></tr>', text)
 if architecture == None:
-    print "No System info found."
+    print("No System info found.")
     exit(0)
 architecture = architecture.group(1).split()[-1]
 
@@ -69,17 +72,17 @@ if not php_greater_74:
 
 
 # Logging
-print "PHP version : " + php_version
-print "Zend Extension ID : " + zend_extension_id
-print "Zend Bin ID : " + zend_bin_id
-print "Assuming " + architecture + " architecture"
+print("PHP version : " + php_version)
+print("Zend Extension ID : " + zend_extension_id)
+print("Zend Bin ID : " + zend_bin_id)
+print("Assuming " + architecture + " architecture")
 
-digest = md5(php_version + zend_extension_id + zend_bin_id).hexdigest()
-print "------------"
-print "System ID : " + digest
+digest = md5(php_version + zend_extension_id + zend_bin_id)
+print("------------")
+print("System ID : " + digest)
 
 if not php_greater_74:
-    alt_digest = md5(php_version + zend_extension_id + alt_zend_bin_id).hexdigest()
-    print "PHP lower than 7.4 detected, an alternate Bin ID is possible:"
-    print "Alternate Zend Bin ID : " + alt_zend_bin_id
-    print "Alternate System ID : " + alt_digest
+    alt_digest = md5(php_version + zend_extension_id + alt_zend_bin_id)
+    print("PHP lower than 7.4 detected, an alternate Bin ID is possible:")
+    print("Alternate Zend Bin ID : " + alt_zend_bin_id)
+    print("Alternate System ID : " + alt_digest)
